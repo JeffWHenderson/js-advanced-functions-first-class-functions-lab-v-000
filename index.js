9f225ab27fbd2f1104f0b522323abda3eb5aa5b8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 16:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const returnLastTwoDrivers = function(drivers) { return drivers.slice(-2) }
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
 
 function createFareMultiplier(multiplier) {
-  return function(multiplier, fare) { multiplier * fare }
+  return function(fare) { multiplier * fare }
  }
